a45bcb49c4359bfab74ee54673193caa4cec77d6
Omar-elrayes327 <omarelrayes3600@gmail.com>
2026-01-02 01:12:35 +0200

    Delete logger (1).py

--- logger (1).py
+++
@@ -1,7 +0,0 @@
-import os
-from datetime import datetime
-from config import LOG_FILE
-
-def log_action(message):
-    with open(LOG_FILE, "a", encoding="utf-8") as log:
-        log.write(f"[{datetime.now()}] {message}\n")
